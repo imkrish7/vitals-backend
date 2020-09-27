fe43b63815e80732fd2689137d4e90380ad01f64
krishna <singhkrishk07@gmail.com>
2020-09-28 04:12:03 +0530

    minor change

--- routes/routes.js
+++ routes/routes.js
@@ -73,7 +73,7 @@ route.get('/temperature', async (req, res) => {
 	var end = new Date(date);
 	end.setHours(23,59,59,999);
 	try {
-		const data = await Temperature.find({date: { $gte: start, $lte: end}});
+		const data = await Temperature.find({date: { $gte: start.now(), $lte: end.now()}});
 		console.log(data)
 		return res.json({ success: true,data: data});
 	} catch (e) {
